13dc2a7b26576fe7a18d5a8d2d450554b92febba
admin <admin@DESKTOP-OTU1QTJ>
2019-11-27 09:58:56 +0530

    This is the create Project Message

---
+++ Create Project/src/com/employee/service/EmployeeService.java
@@ -0,0 +1,8 @@
+package com.employee.service;
+
+import com.employee.bean.EmployeeBean;
+
+public interface EmployeeService {
+	public abstract void createEmployee(EmployeeBean employeeBean);
+
+}
---
+++ Create Project/src/com/employee/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,14 @@
+package com.employee.service.impl;
+
+import com.employee.bean.EmployeeBean;
+import com.employee.service.EmployeeService;
+
+public class EmployeeServiceImpl implements EmployeeService {
+
+	@Override
+	public void createEmployee(EmployeeBean employeeBean) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
